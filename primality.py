a00ab522d0f4c748b203c4465b4b83341a3f3d5a
Daniel Montero <monterodanieleduardo@gmail.com>
2020-11-08 17:09:02 -0500

    Validación de numero primo ¿True or False?

--- primality.py
+++ primality.py
@@ -1,3 +1,13 @@
+
+def primalidad(number):
+    for i in range(2,number+1):
+        if i != 1 and i != number:
+            if number % i != 0:
+                continue
+            else:
+                return False
+    return True        
+
 def run():
     number = input('Please insert a number: ')
     try:
@@ -5,7 +15,11 @@ def run():
     except:
         print('Please, insert only integer numbers.')
         quit
-    
+    if primalidad(number) == False:
+        print('The number is not a prime number')
+    else:
+        print('The number is a prime number')
+
 
 if __name__ == "__main__":
     run()
